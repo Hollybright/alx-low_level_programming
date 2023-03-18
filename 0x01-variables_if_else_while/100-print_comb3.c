654594e4897aed78ac2ef8efbd3d73310f6c911e
hollybright <brightrobinson.official@gmail.com>
2023-03-18 18:01:55 +0000

    Inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0 ; i <= 9 ; i++)
+	for (i = 0 ; i < 9 ; i++)
 	{
-		for (j = i + 1 ; j <= 10 ; j++)
+		for (j = i + 1 ; j < 10 ; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
 
-			if (i != 9 && j != 10)
+			if (i == 9 && j == 10)
 			{
 				putchar(',');
 				putchar(' ');
