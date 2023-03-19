57fc56a559269b3a8bf667ff6806e87a6a75282c
hollybright <brightrobinson.official@gmail.com>
2023-03-19 11:11:34 +0000

    task-6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,5 @@ int main(void)
 	printf("size of a long int: %lu byte(s)", sizeof(long int));
 	printf("size of a long long int: %lu byte(s)", sizeof(long long int));
 	printf("size of a float: %lu byte(s)", sizeof(float));
-	print('\n');
 	return (0);
 }
