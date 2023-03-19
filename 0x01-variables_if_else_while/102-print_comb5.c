5c0939cf5ac0756940c27d528a35619b39c58b00
hollybright <brightrobinson.official@gmail.com>
2023-03-19 08:02:37 +0000

    The success

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combination
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 98 ; i++)
+	{
+		for (j = 1 ; j < 99 ; j++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 10 && j != 10)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
