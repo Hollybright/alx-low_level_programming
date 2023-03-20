00feede953d29889054045aab7b20809df74638f
hollybright <brightrobinson.official@gmail.com>
2023-03-20 14:30:32 +0000

    Task-12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,20 +14,17 @@ int main(void)
 	{
 		for (j = 1 ; j < 99 ; j++)
 		{
-			if (i < j)
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 98 && j != 99)
 			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
+				putchar(',');
 				putchar(' ');
-				putchar((j / 10) + '0');
-				putchar((j % 10) + '0');
-
-				if (i != 98 && j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
+
 		}
 	}
 	putchar('\n');
