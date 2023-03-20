004d946804e04afb4e0475a4137f98a72fa8abd7
hollybright <brightrobinson.official@gmail.com>
2023-03-20 22:27:16 +0000

    Task-12

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - using write, outputs a string to stderr
+ *
+ * Return: (1) successful exit
+ */
+
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\"n, 59);
+
+	return (1);
+}
