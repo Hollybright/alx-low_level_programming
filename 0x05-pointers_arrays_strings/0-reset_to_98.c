76a81ec1832cf0eecda49db30374c8310f16facc
hollybright <brightrobinson.official@gmail.com>
2023-03-27 16:47:27 +0000

    Battery

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
  * reset_to_98 - changes the value of n to 98
- * @n: is the value to be replace
+ * @n: is the value to be replaced
  * Return: 0
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
