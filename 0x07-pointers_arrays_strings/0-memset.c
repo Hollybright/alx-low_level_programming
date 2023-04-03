e951cc9087f8805efbdd603b100282aa3ca67fec
hollybright <brightrobinson.official@gmail.com>
2023-04-03 17:17:52 +0000

    Memset task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - fill a block of memory
+ * @s: starting address of memory
+ * @b: the desire value
+ * @n: number of bytes to be changed
+ *
+ * Return: change array with new value for n bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0 ; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+
+	return (s);
+}
