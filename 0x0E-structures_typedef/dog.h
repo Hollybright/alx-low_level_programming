23b312bf9ab5352942546fd52367582be4c87337
hollybright <brightrobinson.official@gmail.com>
2023-04-18 17:05:59 +0100

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - The basic information of the dog
+ * @name: Member one
+ * @age: Member two
+ * @owner: Member three
+ *
+ * Description: Description terms
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for the struct dog
+ */
+
+typedef struct dog dog_t;
+
+
+#endif
